e5312f802f420afd8f6c3afdb1fca8ae2f397570
taufan269 <tfan8101@gmail.com>
2023-02-18 22:15:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const SSH = require('simple-ssh');
 
 const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
   authStrategy: new LocalAuth()
 });
 
